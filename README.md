b368046a6e1f59d2286bc3941bc3b4d5d4b132ce
Antares <antares.mi@outlook.com>
2025-03-26 14:35:33 +0800

    Update Ubuntu version (#1)

--- README.md
+++ README.md
@@ -1,36 +1,36 @@
 # Sandbox RootFS
-This is the sandbox's rootfs used by [lyrio-judge](https://github.com/lyrio-dev/judge). It's based on Ubuntu 22.04 and contains compilers (and interpreters) below:
 
-* GCC 12
-* Clang 14 (from [LLVM](https://apt.llvm.org/))
-* OpenJDK 17
+This is a fork of [lyrio-dev/sandbox-rootfs](https://github.com/lyrio-dev/sandbox-rootfs).
+
+Thanks to [lyrio-dev](https://github.com/lyrio-dev) and [Menci](https://github.com/menci).
+
+## Description
+
+This is the sandbox's rootfs used by [lyrio-judge](https://github.com/lyrio-dev/judge). It's based on Ubuntu 24.04 and contains compilers (and interpreters) below:
+
+* GCC 14
+* Clang 20 (from [LLVM](https://apt.llvm.org/))
+* OpenJDK 21
 * Kotlin (from [SDKMAN!](https://kotlinlang.org/docs/tutorials/command-line.html))
-* Free Pascal 3
-* Python 2.7
-* Python 3.9
-* Python 3.10
-* Swift (from [Swift.org](https://swift.org/))
+* Python 3.13 (from [PPA](https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa))
 * Rust (from [Rustup](https://rustup.rs/))
 * Go (from [PPA](https://launchpad.net/~longsleep/+archive/ubuntu/golang-backports))
-* GHC 9.0.1 (from [PPA](https://launchpad.net/~hvr/+archive/ubuntu/ghc))
-* C# (from [Mono](https://www.mono-project.com/download/stable/))
-* F# (from [Mono](https://www.mono-project.com/download/stable/))
 
 Each compiler (or interpreter) is available in `$PATH`. It also contains [`testlib.h`](https://github.com/MikeMirzayanov/testlib) in `/usr/include`.
 
 You can download it from [release](https://github.com/lyrio-dev/sandbox-rootfs/releases) or bootstrap by yourself.
 
-# Bootstrapping
+## Bootstrapping
 You'll need:
 
 * A Linux box with root privilege
 * `arch-chroot` (usually in the package `arch-install-scripts`)
-* `debootstrap` (some old version of Debian's `debootstrap` couldn't bootstrap Ubuntu 22.04)
+* `debootstrap` (some old version of Debian's `debootstrap` couldn't bootstrap Ubuntu 24.04)
 
 First, clone this repo:
 
 ```bash
-git clone git@github.com:lyrio-dev/sandbox-rootfs.git
+git clone https://github.com/tywzoj/sandbox-rootfs.git
 cd sandbox-rootfs
 ```
 
--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-GCC_VERSION="12"
-LLVM_VERSION="14"
-OPENJDK_VERSION="17"
-GHC_VERSION="9.0.1"
+GCC_VERSION="14"
+LLVM_VERSION="20"
+OPENJDK_VERSION="21"
+PYTHON_VERSION="3.13"
+GOLANG_VERSION="1.23"
 
 UBUNTU_CODENAME="$(source /etc/os-release && echo "$UBUNTU_CODENAME")"
 UBUNTU_VERSION="$(source /etc/os-release && echo "$VERSION_ID")"
@@ -32,21 +33,15 @@ apt-get install -y gnupg ca-certificates curl wget locales unzip zip git
 
 # Key: LLVM repo
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-# Key: Python repo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BA6932366A755776
+# Key: Python3 repo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys F23C5A6CF475977595C89F51BA6932366A755776
 # Key: Go repo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys F6BC817356A3D45E
-# Key: Haskell repo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys FF3AEACEF6F88286
-# Key: Mono repo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 876B22BA887CA91614B5323FC631127F87FA12D1
 
 # Add sources
-echo "deb http://apt.llvm.org/$UBUNTU_CODENAME/ llvm-toolchain-$UBUNTU_CODENAME-$LLVM_VERSION main" > /etc/apt/sources.list.d/llvm.list
-echo "deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/python.list
-echo "deb http://ppa.launchpad.net/longsleep/golang-backports/ubuntu $UBUNTU_CODENAME main" >  /etc/apt/sources.list.d/go.list
-echo "deb http://ppa.launchpad.net/hvr/ghc/ubuntu focal main" > /etc/apt/sources.list.d/haskell.list
-echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" > /etc/apt/sources.list.d/mono.list
+echo "deb https://apt.llvm.org/$UBUNTU_CODENAME/ llvm-toolchain-$UBUNTU_CODENAME-$LLVM_VERSION main" > /etc/apt/sources.list.d/llvm.list
+echo "deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/python.list
+echo "deb https://ppa.launchpadcontent.net/longsleep/golang-backports/ubuntu $UBUNTU_CODENAME main" >  /etc/apt/sources.list.d/go.list
 
 # Install some language support via APT
 apt-get update
@@ -56,14 +51,8 @@ apt-get install -y g++-$GCC_VERSION-multilib \
                    libc++-$LLVM_VERSION-dev \
                    libc++abi-$LLVM_VERSION-dev \
                    openjdk-$OPENJDK_VERSION-jdk \
-                   fpc \
-                   python2.7 \
-                   python3.9 \
-                   python3.10 \
-                   golang-go \
-                   ghc-$GHC_VERSION \
-                   mono-devel \
-                   fsharp
+                   python$PYTHON_VERSION \
+                   golang-$GOLANG_VERSION 
 
 # Install Rust via Rustup
 su sandbox -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
@@ -72,35 +61,18 @@ su sandbox -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s
 su sandbox -c "curl -s https://get.sdkman.io | bash"
 su sandbox -s /bin/bash -c "source ~/.sdkman/bin/sdkman-init.sh && sdk install kotlin"
 
-# Install Swift
-SWIFT_URL_QUOTED="$(curl https://www.swift.org/download/ --compressed | grep -P "\"([^\"]+ubuntu$UBUNTU_VERSION.tar.gz)\"" -o | head -n 1)"
-SWIFT_URL="$(eval "echo $SWIFT_URL_QUOTED")"
-wget -O - "$SWIFT_URL" | tar -xzf - -C /opt
-mv /opt/swift* /opt/swift
-
 # Create symlinks for compilers and interpreters with non-common names and locations
 ln -s /usr/bin/g++-$GCC_VERSION /usr/local/bin/g++
 ln -s /usr/bin/gcc-$GCC_VERSION /usr/local/bin/gcc
 ln -s /usr/bin/clang-$LLVM_VERSION /usr/local/bin/clang
 ln -s /usr/bin/clang++-$LLVM_VERSION /usr/local/bin/clang++
+ln -s /sandbox/.cargo/bin/rustc /usr/local/bin/rustc
 ln -s /sandbox/.sdkman/candidates/kotlin/current/bin/kotlin /usr/local/bin/kotlin
 ln -s /sandbox/.sdkman/candidates/kotlin/current/bin/kotlinc /usr/local/bin/kotlinc
-ln -s /sandbox/.cargo/bin/rustc /usr/local/bin/rustc
-ln -s /opt/swift/usr/bin/swiftc /usr/local/bin/swiftc
-
-# Create wrapper for GHC
-cat > /usr/local/bin/ghc <<EOF
-#!/bin/bash
-for DIR in /opt/ghc/*/lib/ghc-*/*; do
-    export LD_LIBRARY_PATH="\$LD_LIBRARY_PATH:\$DIR"
-done
-/opt/ghc/bin/ghc "\$@"
-EOF
-chmod +x /usr/local/bin/ghc
 
 # Clean the APT cache
 apt-get clean
 
 # Install testlib
-git clone https://github.com/lyrio-dev/testlib /tmp/testlib
+git clone https://github.com/tywzoj/testlib.git /tmp/testlib
 cp /tmp/testlib/testlib.h /usr/include/
